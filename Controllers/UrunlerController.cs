8bd1f17cb89dd47887bbfc516c322d70f11f42e5
sezginads <adassezgin@gmail.com>
2023-12-14 12:15:53 +0100

    Add project files.

---
+++ Controllers/UrunlerController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcStok.Models.Entity;
+
+namespace MvcStok.Controllers
+{
+    public class UrunlerController : Controller
+    {
+        // GET: Urunler
+
+        MvcDbStokEntities1 db = new MvcDbStokEntities1 ();
+        public ActionResult Index()
+        {
+            var degerler = db.TBLURUNLER.ToList ();
+            return View(degerler);
+        }
+    }
+}
